2e4d5dca2ca34dd843f087fe9768267ac6ea8421
Evan <47926489+Evanz142@users.noreply.github.com>
2024-01-19 23:17:09 -0500

    Initialization
    Frontend and backend sections both running with Expo and Node respectively.

---
+++ split-the-bill-expo/App.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { Button, Image, View } from 'react-native';
+import {launchImageLibrary, launchCamera} from 'react-native-image-picker';
+
+
+const App = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const openImagePicker = () => {
+    const options = {
+      mediaType: 'photo',
+      includeBase64: false,
+      maxHeight: 2000,
+      maxWidth: 2000,
+    };
+
+    launchImageLibrary(options, (response) => {
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('Image picker error: ', response.error);
+      } else {
+        let imageUri = response.uri || response.assets?.[0]?.uri;
+        setSelectedImage(imageUri);
+      }
+    });
+  };
+  
+  handleCameraLaunch = () => {
+    const options = {
+      mediaType: 'photo',
+      includeBase64: false,
+      maxHeight: 2000,
+      maxWidth: 2000,
+    };
+  
+    launchCamera(options, response => {
+      console.log('Response = ', response);
+      if (response.didCancel) {
+        console.log('User cancelled camera');
+      } else if (response.error) {
+        console.log('Camera Error: ', response.error);
+      } else {
+        // Process the captured image
+        let imageUri = response.uri || response.assets?.[0]?.uri;
+        setSelectedImage(imageUri);
+        console.log(imageUri);
+      }
+    });
+  }
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center' }}>
+     {selectedImage && (
+          <Image
+            source={{ uri: selectedImage }}
+            style={{ flex: 1 }}
+            resizeMode="contain"
+          />
+    )}
+    <View style={{ marginTop: 20 }}>
+      <Button title="Choose from Device" onPress={openImagePicker} />
+    </View>
+    <View style={{ marginTop: 20,marginBottom: 50 }}>
+      <Button title="Open Camera" onPress={handleCameraLaunch} />
+    </View>
+  </View>
+  );
+};
+
+export default App;
