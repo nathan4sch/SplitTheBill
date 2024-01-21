51cfc6ee72aa5cda890d76d2627277bff12b10c1
Tyler Pautler <31872040+Tyler16@users.noreply.github.com>
2024-01-21 03:49:26 -0500

    Update server.mjs

--- split-the-bill-backend/server.mjs
+++ split-the-bill-backend/server.mjs
@@ -1,24 +1,36 @@
 import http from 'node:http';
+import fs from 'node:fs';
 import formidable from 'formidable';
-import fs from 'node:fs'
+import receive_data from './apis.js';
 
 const server = http.createServer((req, res) => {
+  console.log("Request Recieved");
+
+  // Check API endpoint
   if (req.url === '/api/upload' && req.method.toLowerCase() === 'post') {
-    // parse a file upload
-    console.log(req);
     const form = formidable({});
 
+    // Get image file
     form.parse(req, (err, fields, files) => {
       if (err) {
         res.writeHead(err.httpCode || 400, { 'Content-Type': 'text/plain' });
         res.end(String(err));
         return;
       }
-      console.log(files.multipleFiles[0].filepath);
-      fs.rename(files.multipleFiles[0].filepath, 'C:\\Users\\evman\\car.png', (err) => {
+
+      console.log("File Info:\n" + files.photo[0].filepath);
+
+      // Add file to correct directory
+      fs.rename(files.multipleFiles[0].filepath, '{Save Directory Here}', (err) => {
         if (err) throw err;
-        console.log("YEAH BABYYY");
+        console.log("File Successfully Saved");
       });
+
+      // Get Teseract and GPT results
+      let api_res = await receive_data();
+      console.log("API Results: " + api_res);
+      
+      // Return API info
       res.writeHead(200, { 'Content-Type': 'application/json' });
       res.end(JSON.stringify({ fields, files }, null, 2));
     });
@@ -26,13 +38,13 @@ const server = http.createServer((req, res) => {
     return;
   }
 
-  // show a file upload form
+  // Generate test web page
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end(`
-    <h2>With Node.js <code>"http"</code> module</h2>
+    <h2>File Upload Test Server</h2>
     <form action="/api/upload" enctype="multipart/form-data" method="post">
-      <div>Text field title: <input type="text" name="title" /></div>
-      <div>File: <input type="file" name="multipleFiles" /></div>
+      <div> Title: <input type="text" name="title" /></div>
+      <div>File: <input type="file" name="photo" /></div>
       <input type="submit" value="Upload" />
     </form>
   `);
